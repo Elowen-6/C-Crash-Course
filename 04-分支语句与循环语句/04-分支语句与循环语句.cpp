0e5f84bf04d9549f7c2d15fd3681908beaa7fee4
siriusxmr <xmr183729@163.com>
2026-07-02 14:01:27 +0800

    项目

--- 04-分支语句与循环语句/04-分支语句与循环语句.cpp
+++ 04-分支语句与循环语句/04-分支语句与循环语句.cpp
@@ -66,6 +66,13 @@ int main()
 	
 	// === 范围for（遍历容器最简洁的方式） ===
 	vector<string> weapons = { "剑", "弓", "法杖","斧"};
+
+	auto hp = 100; // hp是一个整型变量，auto会根据初始化值自动推导类型
+	const int MAX_HP = 100; // 常量，不能被修改
+	const auto MAX_MP = 50;  // 常量，类型由初始化值自动推导
+	int& hpRef = hp; // 引用类型,hpRef是hp的别名(本质上是同一个变量),对hpRef的修改会直接修改hp
+
+	// 引用类型好处，方便快速的遍历容器，如果不使用引用类型，就需要复制容器里的每一个数据，数据复制的过程相当于搬家，耗费性能，引用类型相当于给容器里的每个数据起了个别名，通过w遍历每一个数据。
 	cout << "武器列表";
 	for (const auto& w:weapons) {
 		cout << w << " ";
